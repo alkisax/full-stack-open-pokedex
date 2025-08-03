7ff769e97350a5d3a76e546ce684ea86568225ea
Alkis <alkisax@gmail.com>
2025-08-03 22:12:47 +0300

    working on 11.9

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,15 @@
 describe('Pokedex', function() {
-  const baseUrl = Cypress.env('APP_ENV') === 'ci'
-    ? 'http://localhost:5000'
-    : 'http://localhost:8080'
+
+  // 'http://localhost:5000'
+  // 'http://localhost:8080'
 
   it('front page can be opened', function() {
-    cy.visit(baseUrl)
+    cy.visit('http://localhost:5000')
     cy.contains('bulbasaur')
   })
 
   it('front page can be opened', function() {
-    cy.visit(baseUrl)
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })  
