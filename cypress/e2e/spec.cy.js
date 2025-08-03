c3f56f8d05aaebd9f87e5d29990c8971bca6bc50
Alkis <alkisax@gmail.com>
2025-08-03 22:20:48 +0300

    working on 11.9

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -12,5 +12,13 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-  })  
+  })
+
+  it('can visit ivasaur page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+
+    cy.contains('chlorophyll')
+
+  })
 })
