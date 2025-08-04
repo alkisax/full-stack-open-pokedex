a4971c05761a9673d1f300d3f3ed5f42ab354da3
Alkis <alkisax@gmail.com>
2025-08-04 09:34:46 +0300

    working on 11.10

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port http://localhost:${PORT}`);
-});
+})
