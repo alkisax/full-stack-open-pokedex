ab628750ac81f449d39168f01498912c28f8adcb
Alkis <alkisax@gmail.com>
2025-08-04 11:56:39 +0300

    11.12

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.static('dist'))
 app.get('/version', (req, res) => {
   res.send('1.0.1')
 })
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
