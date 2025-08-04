2d9fc5844a9d9080fffec7772c249ed9943abc18
Alkis <alkisax@gmail.com>
2025-08-04 11:05:56 +0300

    11.11 οκ

--- app.js
+++ app.js
@@ -7,11 +7,9 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('1.0.0')
+  res.send('1.0.1')
 })
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
-
-// minor change for testing deploy
